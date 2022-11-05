28a97ae78476afb023f1ec373cdd953b06cd6503
lkuuuo <leo8117884@gmail.com>
2022-11-04 19:46:50 -0500

    added index.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div>
+    dasdad dadasd
+  </div>;
 }
 
 export default App;
