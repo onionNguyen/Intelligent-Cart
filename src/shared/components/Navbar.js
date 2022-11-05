0d87c80f496fcdfe3773bdee964cbf46ff55c9d1
Minh Nguyen <nhaatmnguyeen@gmail.com>
2022-11-04 22:58:10 -0500

    adjust NavBar

--- src/shared/components/Navbar.js
+++ src/shared/components/Navbar.js
@@ -1,16 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';  
 import {Nav, Navbar, Container, NavDropdown} from 'react-bootstrap';  
+
 function App() {  
   return (  
     <Navbar bg="" expand="md">  
     <Container>  
-      <Navbar.Brand href="#home">Intelligent Cart</Navbar.Brand>  
+      <Navbar.Brand href="/Home">Intelligent Cart</Navbar.Brand>  
       <Navbar.Toggle aria-controls="basic-navbar-nav" />  
       <Navbar.Collapse id="basic-navbar-nav">  
-        <Nav className="me-auto">  
-          <Nav.Link href="/Home.js">Home</Nav.Link>  
-          <Nav.Link href="#link">Search</Nav.Link>
-          <Nav.Link href="/WishList.js">Wishlist</Nav.Link>  
+        <Nav className="me-auto">    
+          <Nav.Link href="/Navbar">Search</Nav.Link>
+          <Nav.Link href="/WishList">Wishlist</Nav.Link>  
           <NavDropdown title="Profile" id="basic-nav-dropdown">  
             <NavDropdown.Item href="#action/3.1">Dropdown Item 1</NavDropdown.Item>  
             <NavDropdown.Item href="#action/3.2">Dropdown Item 2</NavDropdown.Item>  
