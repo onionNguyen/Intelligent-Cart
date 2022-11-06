689eae8aff1967c960fe9dea31e8d0133f7cf8d4
lkuuuo <leo8117884@gmail.com>
2022-11-06 14:44:46 -0600

    added footer product list prototype and little stlying

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from "./pages/Home";
 import { Fragment } from "react";
-import { useState } from "react";
 
 
 const App = () => {
---
+++ src/components/Footer.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Footer = () => (
+  <div>
+    <Container class="bg-light">
+      <footer className="page-footer font-small pt-5">
+        <div className="container-fluid text-center text-md-left">
+          <div className="row">
+            <div className="col-md-6 mt-md-0 mt-3">
+              <img class="me-2" alt="logo" src="logo.png" width="50px" />
+              <h5 className="text-uppercase p-4">Intelligent Cart</h5>
+            </div>
+
+            <hr className="clearfix w-100 d-md-none pb-0" />
+
+            <div className="col-md mb-">
+              <span>
+                <img class="p-3" src="./Facebook Icon.png" />
+                <img class="p-3" src="./GitHub Icon.png" />
+                <img class="p-3" src="./Instagram Icon.png" />
+                <img class="p-3" src="./Twitter Icon.png" />
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="footer-copyright text-center py-3">
+          © 2020 Copyright:
+          <span>Intelligent Cart</span>
+        </div>
+      </footer>
+    </Container>
+  </div>
+);
+
+export default Footer;
