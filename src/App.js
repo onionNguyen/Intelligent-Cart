3c0bb6be1a5dc9ef63e231a5a2e1156b63cbb3d2
Minh Nguyen <nhaatmnguyeen@gmail.com>
2022-11-06 15:17:44 -0600

    deploying server

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from "./pages/Home";
 import { Fragment } from "react";
-<<<<<<< HEAD
-//import { useState } from "react";
-=======
->>>>>>> 689eae8aff1967c960fe9dea31e8d0133f7cf8d4
 
 
 const App = () => {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,6 @@
 import React from "react";
 import Carousel from "../components/carousel.js"
-<<<<<<< HEAD
-
-=======
 import Footer from "../components/Footer.js"
->>>>>>> 689eae8aff1967c960fe9dea31e8d0133f7cf8d4
 import Product from "../components/Product.js";
 import Navigation from "../shared/components/Navigation.js";
 
