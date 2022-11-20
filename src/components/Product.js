fd1f467327d0f8e5ae1b26cb0fb212db73a9fdcc
Minh Nguyen <nhaatmnguyeen@gmail.com>
2022-11-19 20:21:52 -0600

    Take out User function, npm install Axios

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,8 @@
 import React from "react";
-import { getUser, resetUserSession } from "../service/AuthService.js";
 import Navigation from "../shared/components/Navigation.js";
-import Product_data from "../components/Product_data.json";
+
 
 const Product = (props) => { //create constant Product
-
-    const user = getUser();
-    const name = user!== 'undefined' && user ? user.name: '';
-
 
     const {WithHeader} = props
 
@@ -15,11 +10,11 @@ const Product = (props) => { //create constant Product
       //basic layout. For project 5 will have actual products displayed
     <div>
       {WithHeader && <Navigation />}
-      <Product_data />
-      <Footer />
     </div>
     );
   };
+
+
 
   
 
